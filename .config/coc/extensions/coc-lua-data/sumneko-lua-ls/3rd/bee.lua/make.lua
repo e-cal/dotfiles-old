2dd60c669cd8ae34c07908a7c4ea263eef62e755
ecal <ecallanan04@gmail.com>
2020-12-17 22:07:46 -0500

    remove .config/coc

--- .config/coc/extensions/coc-lua-data/sumneko-lua-ls/3rd/bee.lua/make.lua
+++
@@ -1,6 +0,0 @@
-local lm = require 'luamake'
-
-lm.c = lm.plat == 'msvc' and 'c89' or 'c11'
-lm.cxx = 'c++17'
-
-require(('project.luamake.%s'):format(lm.plat))
--- .config/coc/extensions/coc-lua-data/sumneko-lua-ls/3rd/luamake/main.lua
+++
@@ -1,5 +0,0 @@
-require 'bee'
-local fs = require 'bee.filesystem'
-MAKEDIR = fs.exe_path():parent_path()
-package.path = (MAKEDIR / "scripts" / "?.lua"):string()
-require 'main'
